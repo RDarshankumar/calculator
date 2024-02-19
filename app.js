39ddc9c4df2dc9301ca2ce43a419f3778d75b98f
Darshan Kumar <155427044+RDarshankumar@users.noreply.github.com>
2024-02-19 14:07:19 +0500

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+function pick (val){
+    document.getElementById("a").value +=val;
+}
+
+
+function crl (){
+    document.getElementById("a").value = "";}
+
+
+   function solve() {
+    var x =  document.getElementById("a").value +=val;
+    var y = eval (x);
+    document.getElementById("a") .value=y;
+   }
